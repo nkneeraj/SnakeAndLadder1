4f3fd1896950b7db8d3cf34a71b6f539e2a6e3fe
nkneeraj <nkneerj641@gmail.com>
2020-09-25 17:16:46 +0530

    [NK]Created - UC2 Generating random dice value from 0-6.

--- src/com/CG/SnakeLadder/SnakeAndLadder1.java
+++ src/com/CG/SnakeLadder/SnakeAndLadder1.java
@@ -2,10 +2,13 @@ package com.CG.SnakeLadder;
 
 public class SnakeAndLadder1 {
 	public static void main(String[] args) {
+		
 		System.out.println("Welcome to the game");
 		
 		int start_position = 0;
 		int no_of_players = 1;	
-		System.out.println("Start position:"+start_position);
+		int dice_value = (int)Math.floor(Math.random()*10)%6+1;
+		
+		System.out.println("Start position:"+start_position + "\n" +"Dice value:"+dice_value);
 	}
 }
