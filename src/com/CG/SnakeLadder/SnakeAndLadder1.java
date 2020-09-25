f4e26de13cf90ac7ee9f514146cbcfd26b5312da
nkneeraj <nkneerj641@gmail.com>
2020-09-25 17:11:39 +0530

    [NK]Created - UC1 showing the start position

--- src/com/CG/SnakeLadder/SnakeAndLadder1.java
+++ src/com/CG/SnakeLadder/SnakeAndLadder1.java
@@ -3,5 +3,9 @@ package com.CG.SnakeLadder;
 public class SnakeAndLadder1 {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the game");
+		
+		int start_position = 0;
+		int no_of_players = 1;	
+		System.out.println("Start position:"+start_position);
 	}
 }
