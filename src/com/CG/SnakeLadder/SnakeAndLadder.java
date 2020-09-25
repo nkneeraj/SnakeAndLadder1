63e64486618e964e1d1289af931db2c42a4e0eaf
nkneeraj <nkneerj641@gmail.com>
2020-09-25 17:02:51 +0530

    [NK]Created : Master

--- src/com/CG/SnakeLadder/SnakeAndLadder.java
+++ src/com/CG/SnakeLadder/SnakeAndLadder.java
@@ -3,8 +3,7 @@ package com.CG.SnakeLadder;
 public class SnakeAndLadder {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Welcome to the game");
 	}
 
 }
